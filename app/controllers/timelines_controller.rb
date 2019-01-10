109d749fa6bf11ad18248e6f91d3a32b89c04703
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2019-01-10 14:46:11 +0700

    Add timeline

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,4 +1,18 @@
 class TimelinesController < ApplicationController
+  before_action :find_user, only: :show
+
+  def show
+  end
+
   def index
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by(id: params[:id])
+    if @user.blank?
+      redirect_to root_path, alert: "User not found"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'timelines#index'
+  resources :timelines, only: [:index, :show]
 end
