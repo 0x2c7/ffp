b3b893eacf00d2abc91d10428875d610f929e894
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2019-01-10 15:23:32 +0700

    Add avatar and cover

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,7 +1,22 @@
 class TimelinesController < ApplicationController
-  before_action :find_user, only: :show
+  before_action :find_user, only: [:show, :edit, :update]
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if params[:user][:avatar]
+      @user.avatar.attach(params[:user][:avatar])
+    end
+
+    if params[:user][:cover]
+      @user.cover.attach(params[:user][:cover])
+    end
+
+    redirect_to timeline_path(@user)
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'timelines#index'
-  resources :timelines, only: [:index, :show]
+  resources :timelines, only: [:index, :show, :edit, :update]
 end
